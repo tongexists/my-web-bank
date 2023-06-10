aaa42cdff78c627eaa1dafbc539f86b8e1a97b8a
tong-exists <1917966749@qq.com>
2023-06-10 19:30:55 +0800

    one part of fund-manager page

---
+++ pages/index/fund_manager/FundManager.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="container">
+		<view class="top_line">
+			<view class="left">
+				<image src="" class="profile"></image>
+				<view class="text">
+					哪些赚钱的基金经理，都买了啥？
+				</view>
+			</view>
+			<view class="right">
+				<view class="live">
+					<view class="iconfont icon-live"></view>
+					<view class="live-text">
+						直播
+					</view>
+				</view>
+				<view class="search icon-search iconfont"></view>
+			</view>
+		</view>	
+		<view class="market">
+			<view class="one">
+				<view class="left">
+					行情君
+				</view>
+				<view class="right">
+					大盘正在努力，你怎么看<text class="iconfont icon-arrow-right"></text>
+				</view>
+			</view>
+			<view class="two">
+				<view class="left">
+					<view class="a">
+						<view class="l">
+							上证指数
+						</view>
+						<view class="r iconfont icon-shuangxiang"></view>
+					</view>
+					<view class="b">
+						3231.41
+					</view>
+					<view class="c">
+						+0.55%
+					</view>
+				</view>
+				<view class="block">
+					<view class="half-block">
+						<view class="hb-left">
+							传媒
+						</view>
+						<view class="hb-right">
+							+3.04%
+						</view>
+					</view>
+					<view class="half-block">
+						<view class="hb-left">
+							新能源车
+						</view>
+						<view class="hb-right">
+							+1.58%
+						</view>
+					</view>
+				</view>
+				<view class="block">
+					<view class="half-block">
+						<view class="hb-left">
+							计算机
+						</view>
+						<view class="hb-right">
+							+1.73%
+						</view>
+					</view>
+					<view class="half-block">
+						<view class="hb-left">
+							光伏
+						</view>
+						<view class="hb-right">
+							+1.21%
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="three">
+				<image src="" class="img"></image>
+				<view class="text">
+					什么时候是个头
+				</view>
+			</view>
+			<view class="four">
+				<view class="f-left">
+					本月战况如何？
+				</view>
+				<view class="f-right">
+					分享本月战绩
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script src="./fund_manager_js">
+	
+</script>
+
+<style scoped lang="less" src="./fund_manager_less.less">
+
+</style>
